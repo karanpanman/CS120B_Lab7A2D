1ed403d1d3110169d774aee0e492c0c187f119ac
karanpanman <74081077+karanpanman@users.noreply.github.com>
2021-04-29 19:46:09 -0700

    Update kbhog001_lab7_part2.c

--- kbhog001_lab7_part2.c
+++ kbhog001_lab7_part2.c
@@ -6,7 +6,7 @@
  * I acknowledge all content contained herein, excluding template or example
  * code is my own original work.
  *
- *  Demo Link: Youtube URL>
+ *  Demo Link: https://drive.google.com/file/d/1FHWX7FGnYKgFFtA5Z-zSpnLFyvZk78hQ/view?usp=sharing
  */
 
 #include <avr/io.h>
